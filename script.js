c01b15de5b51ca9da96097f585ae6cb561cae46e
savasgormus <savasgormus1@gmail.com>
2022-03-10 21:18:10 +0300

    update

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-const container = document.querySelector('.container');
+const container = document.querySelector(".container");
+const allSeats = document.querySelectorAll(".container .seat");
+const notOccupiedSeats = document.querySelector(".container .seat:not(.occupied)");
+const count = document.getElementById("count");
+const film = document.getElementById("film");
+const total = document.getElementById("total");
+const movieSelectBox = document.getElementById("select");
