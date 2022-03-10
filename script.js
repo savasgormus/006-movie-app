e1f1419ced7c2bac0253335017a87a8887599052
savasgormus <savasgormus1@gmail.com>
2022-03-10 23:18:53 +0300

    update

--- script.js
+++ script.js
@@ -9,12 +9,13 @@ const movieSelectBox = document.getElementById("movie");
 // önce local storage sonra selectbox
 // initial value == movieSelectBox.value
 // movieSelectBox.options[movieSelectBox.selectedIndex].value == movieSelectBox.value
+// en güncel movie seat price
 let currentTicketPrice = localStorage.getItem
 ("selectedMoviePrice") ? localStorage.getItem
 ("selectedMoviePrice") : movieSelectBox.options
 [movieSelectBox.selectedIndex].value;
 
-// movieIndex
+// movieIndex (en güncel movie index)
 let currentMovieIndex = localStorage.getItem
 ("selectedMovieIndex") ? localStorage.getItem
 ("selectedMovieIndex") : movieSelectBox.selectedIndex;
@@ -22,7 +23,7 @@ let currentMovieIndex = localStorage.getItem
 movieSelectBox.addEventListener("change", (e) => {
     let ticketPrice = e.target.value;
     let movieIndex = e.target.selectedIndex;
-    console.log(movieIndex);
+    // console.log(movieIndex);
     setMovieDataToLocalStorage(ticketPrice, movieIndex );
 });
 
@@ -33,11 +34,23 @@ const setMovieDataToLocalStorage = (ticketPrice, movieIndex) => {
 };
 
 container.addEventListener("click", (e) => {
-    console.log(e.target);
+    // console.log(e.target);
     if (e.target.classList.contains("seat") && !e.target.classList.contains("occupied")) {
         e.target.classList.toggle("selected");
-        console.log(e.target.classList);
+        // console.log(e.target.classList);
     } 
-    // updateMovieInfo();
+    updateMovieInfo();
 });
+
+const updateMovieInfo = () => {
+    let selectedSeats = document.querySelectorAll(".row .seat.selected");
+    let selectedSeatsIndexArray = [...selectedSeats].map(seat => [...allSeats].indexOf(seat));
+    // console.log(selectedSeatsIndexArray);
+    localStorage.setItem("selectedSeats", JSON.stringify(selectedSeatsIndexArray));
+
+    count.innerText = selectedSeatsIndexArray.lenght;
+    total.innerText = selectedSeatsIndexArray.lenght * currentTicketPrice;
+    film.innerText = movieSelectBox.options[movieSelectBox.selectedIndex].innerText.split("(")[0];
+
+};
 
